53f0c49560e7147a0b66102494a8a7b77e2691d8
Spuute <spuute@gmail.com>
2022-01-31 14:05:23 +0100

    chore(server): Adds property for Demo_Product model

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -6,5 +6,6 @@ namespace Tangy_Models.LearnBlazorModels
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
