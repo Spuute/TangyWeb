646d5f6af3fd6edee91d22505126ad5bd755b180
Spuute <spuute@gmail.com>
2022-02-04 17:26:28 +0100

    feat(common): initialise

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using TangyWeb_Server.Data;
+using Microsoft.EntityFrameworkCore;
+using Tangy_DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
